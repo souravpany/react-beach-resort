5a1f15ce9aa96c770e8716e5e4197defbedd35a5
sourav.kumar.pany <sourav.kumar.pany@gds.ey.com>
2022-11-19 18:16:57 +0530

    Add code clen up for room search screen.

--- src/pages/room/RoomSearchScreen.jsx
+++ src/pages/room/RoomSearchScreen.jsx
@@ -24,87 +24,91 @@ function RoomSearchScreen() {
       <Navbar/>
       {/* Top section image with transparent overlapping box */}
       <div className='search-room-top-section'>
-        <img className='search-room-main-logo' src={Room2} alt="Room2 Logo" />
-          <div className='card-trans-container'>
-              <h1>Our Rooms</h1>
-              <div className='line-bar'></div>
-              <Link to='/'>
-              <button className='card-trans-button'>RETURN HOME</button>
-              </Link>
-          </div>
-      </div>
+        <img className='search-room-logo-img' src={Room2} alt="Room2 Logo" />
+            <div className='seach-room-img-middle-section'>
+                <h1>Our Rooms</h1>
+                <div/>
+                <Link to='/'>
+                 <button>RETURN HOME</button>
+                 </Link>
+            </div>
+        </div>
 
       {/* middle section search room with card images*/}
       <div className='middle-section-container'>
         <h1>Search Rooms</h1>
         <div className='search-line-bar'/>
 
-            {/* search room part*/}
-            <div className='middle-section-top-part'>
+          <div className="search-sub-middle-section-container">
 
-              {/* room type section */}
-              <div className='room-type-section'>
-              <text>Rooms Type</text>
-                <select className='title-text' onChange={onOptionChangeHandler}>
-                        <option>Please choose one option</option>
-                          {options.map((option, index) => {
-                              return <option key={index} >
-                                  {option}
-                              </option>
-                          })}
-                </select>
-              </div>
+                {/* search room part*/}
+                <div className='middle-section-top-part'>
 
-
-              {/* Guest type section */}
-              <div className='room-type-section'>
-              <text>Guests</text>
-                <select className='title-text' onChange={onguestOptionChangeHandler}>
-                      <option>Please choose one option</option>
-                        {guestOptions.map((option, index) => {
-                            return <option key={index} >
-                                {option}
-                            </option>
-                        })}
-                </select>
-              </div>
-
-              {/* Room price */}
-              <div className='room-type-section'>
-                <text>Room Price $600</text>
-                <text className='title-text'>Rate View</text>
-              </div>
-              
-              {/* Room size */}
-              <div className='room-type-section'>
-                <text>Room Size</text>
-                  <div className='title-text'>
-                  <input className='room-size-input-text-area' value={0}></input>
-                  <strong>{'  '}</strong>
-                  <input className='room-size-input-text-area' value={1000}></input>
+                  {/* room type section */}
+                  <div className='room-type-section'>
+                  <text>Rooms Type</text>
+                    <select className='title-text' onChange={onOptionChangeHandler}>
+                            <option>Please choose one option</option>
+                              {options.map((option, index) => {
+                                  return <option key={index} >
+                                      {option}
+                                  </option>
+                              })}
+                    </select>
                   </div>
-              </div>
 
-              {/* Check box */}
-              <div className='room-search-check-box'>
 
-                <div className='check-box-section'>
-                <input className='check-box' type="checkbox"/> Breakfast
-                </div>           
+                  {/* Guest type section */}
+                  <div className='room-type-section'>
+                  <text>Guests</text>
+                    <select className='title-text' onChange={onguestOptionChangeHandler}>
+                          <option>Please choose one option</option>
+                            {guestOptions.map((option, index) => {
+                                return <option key={index} >
+                                    {option}
+                                </option>
+                            })}
+                    </select>
+                  </div>
+
+                  {/* Room price */}
+                  <div className='room-type-section'>
+                    <text>Room Price $600</text>
+                    <text className='title-text'>Rate View</text>
+                  </div>
+                  
+                  {/* Room size */}
+                  <div className='room-type-section'>
+                    <text>Room Size</text>
+                      <div className='title-text'>
+                      <input className='room-size-input-text-area' value={0}></input>
+                      <strong>{'  '}</strong>
+                      <input className='room-size-input-text-area' value={1000}></input>
+                      </div>
+                  </div>
+
+                  {/* Check box */}
+                  <div className='room-search-check-box'>
+
+                    <div className='check-box-section'>
+                    <input className='check-box' type="checkbox"/> Breakfast
+                    </div>           
+                    
+                    <div className='check-box-section'>
+                    <input type="checkbox"/> Pets
+                    </div>
                 
-                <div className='check-box-section'>
-                <input type="checkbox"/> Pets
+                  </div>
+
                 </div>
-            
-              </div>
 
-            </div>
+                {/* card images part*/}
+                <div className='card-image-container'>
+                  {roomItems.map((item) =>
+                    <FeaturedRoomCard item={item} key={item.id}/>
+                  )}
+                </div>
 
-            {/* card images part*/}
-            <div className='card-image-container'>
-              {roomItems.map((item) =>
-                <FeaturedRoomCard item={item} key={item.id}/>
-              )}
             </div>
             
       </div>
